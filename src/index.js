53bb7ee60d01720708de7752f38e0c8704b8aaaf
Skurishin Vladislav <vskurishin@marathonbet.ru>
2016-12-25 17:25:02 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -1,99 +1,567 @@
 'use strict';
 
 /**
- * Created by vladthelittleone on 03.06.16.
+ * @since 03.06.16
+ * @author Skurishin Vladislav
  */
-var VK = require('vksdk');
+// load math.js
+var lodash = require('lodash');
+var config = require('./config.json');
 
-var settings = require('./settings');
-var users = require('./users');
-var binding = require('./binding');
+var output = require('./module/output');
+var util = require('util');
 
-var pollParams;
+var ERROR1 = config['error1'];
+var ERROR2 = config['error2'];
+var E = config['E'];
+var PROBABILITIES = config['P(E)'];
 
-var vk = new VK({
-    'appId':     0,
-    'appSecret': '0',
-    'mode':      settings.mode
+var states = new Map();
+var optimums = new Map();
+
+generateImages(states, convert(E), E[0].length);
+
+var i = 1;
+
+// Окончательная табличка (массив) в отсортированном состоянии в соответствии
+// с мощностями множеств состояний.
+var statesArray = [];
+
+states.forEach(function (e) {
+
+	statesArray.push(e);
+
 });
 
-// Turn on requests with access tokens
-vk.setSecureRequests(true);
-vk.setToken(settings.token);
+console.log(output.beginDocument());
 
-// Waiting for special 'serverTokenReady' event
-vk.request('messages.getLongPollServer', {'need_pts': 1, 'use_ssl': 1});
+// Сортируем в соответствии с мощностью множеств состояний.
+var sort = sortByStatesLength(statesArray);
 
-done('messages.send', function (data) {
-    console.log(data);
+var indexOfR = E[0].length + 1;
+sort.forEach(function (e) {
+
+	e.k = indexOfR;
+
+	indexOfR++;
+
 });
 
-done('messages.getLongPollHistory', onLongPollHistoryDone);
-done('messages.getLongPollServer', onLongPollServerDone);
+generateTableOfOptimums(sort);
 
-// =============================================================================================
+// Начинаем отрисовку таблицы с результатами.
+console.log(output.drawHeaderOfResultTable());
 
-function onLongPollServerDone(data) {
-    pollParams = data.response;
+// Начальный индекс R для таблички.
+sort.forEach(function (e, index) {
 
-    vk.request('messages.getLongPollHistory', {'ts': pollParams.ts, 'pts': pollParams.pts});
-}
+	var listIndexE = [];
+	e.states.forEach(function (e) {
+		listIndexE.push(e.num);
+	});
 
-function onLongPollHistoryDone(data) {
+	var listIndexOfCheck = [];
+	e.checks.forEach(function (e) {
+		listIndexOfCheck.push(e + 1);
+	});
 
-    var res = data.response;
+	console.log(output.insertIntoResultTable(index,
+											 e.k,
+											 listIndexE,
+											 listIndexOfCheck,
+											 e.optimum + 1,
+											 e.D));
 
-    if (res) {
+	indexOfR++;
+});
 
-        pollParams.pts = res.new_pts ? res.new_pts : pollParams.pts;
+console.log(output.drawEndOfResultTable());
+console.log(output.endDocument());
 
-        handleMessages(res);
-    }
+function sortByStatesLength (resultArray) {
 
-    vk.request('messages.getLongPollHistory', {'ts': pollParams.ts, 'pts': pollParams.pts});
+	// Сортируем в соответствии с мощностью множеств состояний.
+	return lodash.sortBy(resultArray, function (R) {
 
-}
+		return R.states.length;
 
-function handleMessages(res) {
-
-    var m = res.messages.items;
-
-    m.length && m.forEach(function (m) {
-
-        console.log(m);
-
-        if (!m.out) {
-
-            var user = users.addUser(m);
-            var text = m.body.trim();
-            var command = binding[text.toLocaleLowerCase()] || 'text';
-
-            user.states.setMessageId(m.id);
-
-            markAsRead(m.id);
-
-            sendMessage(m.user_id, user.states[command](text));
-
-        }
-
-    });
+	});
 
 }
 
-function sendMessage(id, message) {
+function generateImages (map, R, checkSize) {
 
-    vk.request('messages.send', {'user_id': id, 'message': message});
+	var images = [];
+
+	if (R.states.length < 2) {
+
+		return;
+
+	}
+
+	map.set(R.states + R.checks, R);
+
+	R.checks.forEach(function (c) {
+
+		var img = newImages(R, c, checkSize);
+
+		images.push(img.Rn);
+		images.push(img.Rp);
+
+	});
+
+	images.forEach(function (Ri) {
+
+		generateImages(map, Ri, checkSize);
+
+	});
+}
+
+function convert (E) {
+
+	var checksCount = E[0].length;
+	var result = R();
+
+	E.forEach(function (Ei, n) {
+
+		result.states.push({
+							   num: n,
+							   errors: Ei
+						   });
+
+	});
+
+	result.checks = Array.from(new Array(checksCount).keys());
+
+	return result;
+}
+
+function generatePermissibleChecks (R, checksCount) {
+
+	for (var i = 0; i < checksCount; i++) {
+
+		var equals = true;
+		var tmpValue = null;
+
+		R.states.forEach(function (E) {
+
+			if (!tmpValue) {
+
+				tmpValue = E.errors[i];
+
+				return;
+
+			}
+
+			if (E.errors[i] !== tmpValue) {
+
+				equals = false;
+
+			}
+
+		});
+
+		if (!equals) {
+
+			R.checks.push(i);
+
+		}
+
+	}
 
 }
 
-function markAsRead(mid) {
+function generateNewImage (Rk, i) {
 
-    vk.request('messages.markAsRead', {'message_ids': mid});
+	var Rp = R();
+	var Rn = R();
+
+	Rk.states.forEach(function (Ei) {
+
+		if (Ei.errors[i] === -1) {
+
+			Rn.states.push(Ei);
+
+		}
+
+		if (Ei.errors[i] === 1) {
+
+			Rp.states.push(Ei);
+
+		}
+
+	});
+
+	return {Rp: Rp, Rn: Rn};
+}
+
+function newImages (Rk, i, checksSize) {
+
+	var newImages = generateNewImage(Rk, i);
+
+	var Rp = newImages.Rp;
+	var Rn = newImages.Rn;
+
+	generatePermissibleChecks(Rp, checksSize);
+	generatePermissibleChecks(Rn, checksSize);
+
+	return {
+
+		Rn: Rn,
+		Rp: Rp
+
+	}
+}
+
+function R () {
+
+	var t = {};
+
+	t.checks = [];
+	t.states = [];
+
+	return t;
 
 }
 
-function done(type, callback) {
+function optionHash (states) {
 
-    vk.on('done:' + type, callback);
+	var hash = '';
 
+	states.forEach(function (e) {
+
+		hash = hash + e.num;
+
+	});
+
+	return hash;
+}
+
+function propagate (e, optimumsMap, optimumsChain) {
+
+	var optimum = 0;
+	var hash = optionHash(e.states);
+
+	if (e.states.length < 2) {
+
+		e.states.forEach(function (el) {
+
+			optimumsMap.set(el.num, optimumsChain);
+
+		});
+
+		return;
+
+	}
+
+	if (optimums.has(hash)) {
+
+		optimum = optimums.get(hash);
+		optimumsChain.add(optimum)
+
+	}
+
+	// изображения (стр 15 пункт 1)
+	var images = newImages(e, optimum);
+
+	var Rn = images.Rn;
+	var Rp = images.Rp;
+
+	propagate(Rn, optimumsMap, new Set(optimumsChain));
+	propagate(Rp, optimumsMap, new Set(optimumsChain));
+
+}
+
+function getOptimumsForChain (e, c) {
+
+	var optimumsMap = new Map();
+	var optimumsChain = new Set();
+
+	// изображения (стр 15 пункт 1)
+	var images = newImages(e, c);
+
+	var Rn = images.Rn;
+	var Rp = images.Rp;
+
+	optimumsChain.add(c);
+
+	propagate(Rn, optimumsMap, new Set(optimumsChain));
+	propagate(Rp, optimumsMap, new Set(optimumsChain));
+
+	return optimumsMap;
+
+}
+
+function conditionalProbabilities (states, optimums, currentCheck, k) {
+
+	var probabilitiesMap = new Map();
+
+	states.forEach(function (state1) {
+
+		// Веротяности наступления i-го события при
+		// диагностировании i-го состояния
+		var validProbabilities = 0;
+
+		// Веротяности наступления i-го события при
+		// диагностировании любого состояния
+		var allProbabilities = [];
+
+		var argumetsForValidProbabilty = {};
+		var argumentsForAllProbabilities = [];
+
+		states.forEach(function (state2) {
+
+			// Вычисляем гамму (см формулу 28)
+			// state1.num = i
+			// state2.num = f
+			// c = j
+			// state1.optimums = множество ik проверкок
+			var mGamma = multiplyGamma(state1.num, state2.num, optimums.get(state1.num), currentCheck, k);
+
+			// Если равны то это верная диагностика
+			// ее мы будем делить на знаментаель (см 15 стр. пункт 3)
+			if (state1.num === state2.num) {
+
+				// Все условные вероятности для данного i
+				// P(i) * P(R^i / Ri)
+
+				validProbabilities = PROBABILITIES[state2.num] * mGamma;
+
+				argumetsForValidProbabilty = {
+					first: PROBABILITIES[state2.num],
+					second: mGamma
+				};
+
+			}
+
+			// Все условные вероятности для данного i
+			// P(f) * P(R^i / Rf)
+
+			allProbabilities.push(PROBABILITIES[state2.num] * mGamma);
+
+			argumentsForAllProbabilities.push({
+												  first: PROBABILITIES[state2.num],
+												  second: mGamma,
+												  stateNum: state2.num
+											  });
+
+		});
+
+		// Записываем вероятность верного диагностирования для state1.num состояния.
+		// (см. 15 стр. пункт 3 и формула 27)
+
+		//	function probability(numerator, denominators, i, k, j, N, result)
+
+		probabilitiesMap.set(state1.num, clarifyProbability(validProbabilities, lodash.sum(allProbabilities)));
+
+		console.log(output.validProbability(argumetsForValidProbabilty,
+											argumentsForAllProbabilities,
+											state1.num,
+											k,
+											state1.num,
+											probabilitiesMap.get(state1.num)));
+
+	});
+
+	return probabilitiesMap;
+
+}
+
+function probabilitiesSum (states) {
+
+	var sum = 0;
+
+	states.forEach(function (state) {
+
+		sum = sum + PROBABILITIES[state.num];
+
+	});
+
+	return sum;
+}
+
+function probabilitiesWithStates (states) {
+
+	var arr = [];
+
+	states.forEach(function (state) {
+
+		var obj = {
+			stateNum: state.num,
+			probability: PROBABILITIES[state.num]
+		};
+
+		arr.push(obj);
+	});
+
+	return arr;
+}
+
+function optimum (e) {
+
+	// Все доступные проверки для текущего
+	// Ri
+	var checks = e.checks;
+	var efficiencyOfChecks = [];
+
+	checks.forEach(function (c) {
+
+		// Пробуем достать уже готовые оптимумы для данных
+		// изображений (см. стр. 15 пункт 2)
+		var optimums = getOptimumsForChain(e, c);
+
+		var probabilitiesMap = conditionalProbabilities(e.states, optimums, c, e.k);
+
+		// 4 и 5 ПУНКТЫ ВЫЧИСЛЕНИЙ
+		var D = 0;
+
+		var sum = probabilitiesSum(e.states);
+
+		var probabilitiesWithStatesObj = probabilitiesWithStates(e.states);
+
+		var bayesValues = [];
+
+		e.states.forEach(function (state) {
+
+			var stateProbability = PROBABILITIES[state.num];
+			var clarify = clarifyProbability(stateProbability, sum);
+
+			console.log(output.clarifyProbability(stateProbability,
+												  probabilitiesWithStatesObj,
+												  e.k,
+												  state.num,
+												  clarify));
+
+			var bayes = probabilitiesMap.get(state.num);
+
+			bayesValues.push({
+								 state: state.num,
+								 first: clarify,
+								 second: bayes
+							 });
+
+			D = D + (clarify * bayes);
+		});
+
+		console.log(output.D(e.k,
+							 c,
+							 D,
+							 bayesValues.map(function (a) {
+								 return {first: a.first, second: a.second};
+							 }),
+							 bayesValues.map(function (a) {
+								 return a.state;
+							 })));
+
+		efficiencyOfChecks.push({
+									check: c,
+									D: D
+								});
+
+	});
+
+	// Находим наиличшую эффективность.
+	var bestEfficiency = lodash.maxBy(efficiencyOfChecks, function (o) {
+		return o.D;
+	});
+
+	// ЭТО ЕСТЬ ОПТИМУМ
+	optimums.set(optionHash(e.states), bestEfficiency.check);
+
+	e.optimum = bestEfficiency.check;
+	e.D = bestEfficiency.D;
+
+}
+
+/**
+ * Генерация оптимумов и их D.
+ *
+ * @param array
+ */
+function generateTableOfOptimums (array) {
+
+	// Проходим по всем данным.
+	array.forEach(function (e) {
+
+		optimum(e);
+
+	});
+
+}
+
+/**
+ * Формула №28 по методическим указаниям.
+ */
+function multiplyGamma (i, f, optimums, currentCheck, k) {
+
+	var multiply = 1;
+
+	var str = "";
+
+	str = str.concat(output.drawBeginOfGamma(k, i, currentCheck, f));
+
+	var j = 0;
+
+	var resultsOfGamma = [];
+	// начинаем разбор нижележащих оптимальных состояний.
+	optimums.forEach(function (optimum) {
+
+		// Условие для вывода знака умножения.
+		if (j > 0) {
+			str = str.concat("*");
+		}
+
+		var resultOfGamma = gamma(i, optimum, f);
+		multiply = multiply * resultOfGamma;
+
+		resultsOfGamma.push(resultOfGamma);
+
+		// Отрисовываем множители выражения.
+		str = str.concat(output.gamma(i, f, optimum));
+
+		j++;
+	});
+
+	str = str.concat(output.multiplyOfArgumets(resultsOfGamma));
+
+	str = str.concat(util.format("=%d\\]", multiply.toFixed(output.PRECISION)));
+
+	console.log(str);
+
+	return multiply;
+
+}
+
+/**
+ * Формула №25 по методическим указаниям.
+ */
+function clarifyProbability (probabilityOfCurrentState,
+							 probabilitiesForClarify) {
+
+	return probabilityOfCurrentState / probabilitiesForClarify;
+
+}
+
+/**
+ * Формула №29 по методическим указаниям.
+ */
+function gamma (i, j, f) {
+
+	var Eij = E[i][j];
+	var Efj = E[f][j];
+
+	// Если eij == efj
+	if (lodash.isEqual(Eij, Efj)) {
+
+		// если Если eij == efj == 1 -> (1 - alpha(j)) (alpha - ошибка первого рода)
+		// если Если eij == efj == -1 -> (1 - betta(j)) (betta - ошибка второго рода)
+		return lodash.isEqual(Eij, 1) ? 1 - ERROR1[j] :
+		1 - ERROR2[j];
+
+	}
+
+	// если eij == -1 И efj == 1 -> alpha(j)
+	// если eij == 1 И efj == -1 -> betta(j)
+	return lodash.isEqual(Eij, -1) && lodash.isEqual(Efj, 1) ? ERROR1[j] :
+		ERROR2[j];
 }
---
+++ src/module/output.js
@@ -0,0 +1,334 @@
+/**
+ * Created by greezlock on 24.12.2016.
+ */
+
+module.exports = output();
+
+var util = require('util');
+var lodash = require('lodash');
+
+var config = require('./../config.json');
+
+var ERROR1 = config['error1'];
+var ERROR2 = config['error2'];
+var E = config['E'];
+
+function output(fileName) {
+
+	var that = {};
+
+	var PRECISION = 2;
+
+	that.drawHeaderOfResultTable = drawHeaderOfResultTable;
+	that.insertIntoResultTable = insertIntoResultTable;
+	that.drawEndOfResultTable = drawEndOfResultTable;
+	that.drawBeginOfGamma = drawBeginOfGamma;
+	that.gamma = gamma;
+	that.multiplyOfArgumets = multiplyOfArgumets;
+	that.validProbability = validProbability;
+	that.clarifyProbability = clarifyProbability;
+	that.D = D;
+
+	that.PRECISION = PRECISION;
+
+	that.beginDocument = beginDocument;
+	that.endDocument = endDocument;
+
+	return that;
+
+	function beginDocument() {
+
+		var str = "\\documentclass{article} \\usepackage[english]{babel} \\usepackage{amssymb} " +
+				  "\\usepackage{amsmath} \\usepackage{txfonts} \\usepackage{mathdots} " +
+				  "\\usepackage[classicReIm]{kpfonts} \\usepackage[pdftex]{graphicx} " +
+				  "\\begin{document} ";
+
+		return str;
+	}
+
+	function drawHeaderOfResultTable() {
+
+		var str = "\\begin{tabular}{|p{0.3in}|p{1.5in}|p{1.5in}|p{0.6in}|p{0.6in}|} \\hline " +
+				  "№ пп & Информационные состояния \\textit{R${}_{k}$} & Множества $\\Pi$\\textit{${}_{k }$}" +
+				  "допустимых проверок в состояниях \\textit{R${}_{k}$} & \\multicolumn{2}{|p{1.2in}|}" +
+				  "{Результаты расчетов } \\\\ \\hline &  &  & Оптимальная проверка $\\piup$\\textit{${}_{j}$} " +
+				  "& \\textit{D}(\\textit{R${}_{k}$}) \\\\" +
+				  "\\hline 1 & 2 & 3 & 4 & 5 \\\\";
+
+		return str;
+	}
+
+	function insertIntoResultTable(currentRowNumber,
+								   indexR,
+								   listIndexOfE,
+								   listIndexCheck,
+								   optimus,
+								   D) {
+		var str = util.format("\\hline %d & \\textit{R}${}_{%d}$=$\\{$",
+							  currentRowNumber,
+							  indexR);
+
+		str = str.concat(generateListOf(listIndexOfE));
+
+		str = str.concat(util.format("$$\\}$ & $\\Pi$${}_{%d}$=$\\{$", indexR));
+
+		listIndexCheck.forEach(function (value, index) {
+
+			if (index != 0) {
+				str = str.concat("$,");
+			}
+
+			str = str.concat(util.format("$\\piup$${}_{%d}", value));
+		});
+
+		str = str.concat("$$\\}$");
+
+		str = str.concat(util.format("& $\\piup$${}_{%d}$ & %d \\\\", optimus, D.toFixed(PRECISION)));
+
+		return str;
+	}
+
+	function generateListOf(listIndexOfE) {
+
+		var str = "";
+
+		listIndexOfE.forEach(function (value, index) {
+
+			// Разделитель элементов в списке E
+			if (index !== 0) {
+				str = str.concat("$,");
+			}
+
+			str = str.concat(util.format("\\textbf{ E}${}_{%d}", value));
+		});
+
+		return str;
+	}
+
+	function drawEndOfResultTable() {
+
+		return "\\hline \\end{tabular}";
+
+	}
+
+	function endDocument() {
+
+		return "\\end{document}";
+
+	}
+
+	function efficientOfCheck(indexOfCheck,
+							  negativeFlag,
+							  indexOfCurrentR,
+							  indexOfPreviousR,
+							  listIndexOfE) {
+
+		var p = negativeFlag === true ? -1 : 1;
+
+		var str = "\\noindent $\\pi _{%d}^{1} $:";
+		str = str.concat(generateListOf(listIndexOfE));
+		str = str.concat(util.format("\\noindent $\\pi _{%d}^{%d} $:\\textit{R}${}_{%d}$\\textit{$\to$} \\textit{R}${}_{%d}$=$\\{$",
+									 indexOfCheck,
+									 p,
+									 indexOfCurrentR,
+									 indexOfPreviousR));
+		str = str.concat(generateListOf(listIndexOfE));
+
+		str = str.concat("$$\}$");
+
+		return str;
+	}
+
+	// Начало формулы 28.
+	function drawBeginOfGamma(k, i, optimus, f) {
+
+		return util.format("\\[{\\rm P}_{%d} \\left(R_{%d}^{*} (\\pi _{%d} )/R_{%d} \\right)=",
+						   k,
+						   i,
+						   optimus,
+						   f);
+
+	}
+
+	function gamma(i, j, f) {
+
+		var Eij = E[i][j];
+		var Efj = E[f][j];
+
+		// Если eij == efj
+		if (lodash.isEqual(Eij, Efj)) {
+
+			// если Если eij == efj == 1 -> (1 - alpha(j)) (alpha - ошибка первого рода)
+			// если Если eij == efj == -1 -> (1 - betta(j)) (betta - ошибка второго рода)
+			return lodash.isEqual(Eij, 1) ? util.format("(1-\\alpha _{%d})", j) :
+				   util.format("(1-\\beta _{%d})", j);
+
+		}
+
+		// если eij == -1 И efj == 1 -> alpha(j)
+		// если eij == 1 И efj == -1 -> betta(j)
+		return lodash.isEqual(Eij, -1) && lodash.isEqual(Efj, 1) ? util.format("(\\alpha _{%d})", j) :
+			   util.format("(\\beta _{%d})", j);
+
+	}
+
+	function multiplyOfArgumets(arguments) {
+
+		var str = "=";
+
+		arguments.forEach(function (value, index) {
+
+			if (index > 0) {
+
+				str = str.concat("*");
+			}
+
+			str = str.concat(value.toFixed(PRECISION));
+		});
+
+		return str;
+	}
+
+	// Олег
+	function createDenominator(denominators) {
+
+		var denominatorTemplate = "DENOMINATORS_FIRST \\cdot DENOMINATORS_SECOND +";
+		var denominatorResult = "";
+		denominators.forEach(function (value) {
+			denominatorResult += (denominatorTemplate.replace("DENOMINATORS_FIRST", value.first.toFixed(PRECISION))
+				.replace("DENOMINATORS_SECOND", value.second.toFixed(PRECISION)));
+		});
+		// дропаем лишний +
+		return denominatorResult.substring(0, denominatorResult.length - 1);
+	}
+	
+	/**
+	 * Олег
+	 * @param numerator числитель объект с двумя полями
+	 * numerator.first и numerator.second
+	 * @param i индекс для P
+	 * @param k индекс для R
+	 * @param j индекс R*
+	 * @param N до какого значения должна выисляться сумму в знаменателе
+	 * @returns {string}
+	 */
+	function validProbability(numerator, denominators, i, k, j, result) {
+
+		var listOfIndex = denominators.map(function (a) {
+			return a.stateNum;
+		});
+
+		var resultString = "\\[{\\rm P}_{INDEX_K} \\left(R_{INDEX_I} /R_{INDEX_I}^{} (\\pi _{INDEX_J} )\\right)=" +
+						   "\\frac{{\\rm P}(R_{INDEX_I} ){\\rm P}_{INDEX_K} \\left(R_{INDEX_I}^{} (\\pi {INDEX_J} )/R_{INDEX_I} \\right)}" +
+						   "{SUM} =\\]" +
+						   "\\[~=~\\frac{NUMERATOR_FIRST \\cdot NUMERATOR_SECOND}{DENOMINATOR} =RESULT.\\]";
+		// ЗАМЕНА В ФОРМУЛУ В ОБЩЕМ ВИДЕ
+		resultString = resultString.replace('RESULT', result.toFixed(PRECISION));
+		resultString = resultString.replace('SUM', createSumP3(listOfIndex, k, j));
+		resultString = resultString.replace(/INDEX_I/g, i);
+		resultString = resultString.replace(/INDEX_K/g, k);
+		resultString = resultString.replace(/INDEX_J/g, j);
+		// ПОДСТАНОВКА В ЧИСЛИТЕЛЬ
+		resultString = resultString.replace('NUMERATOR_FIRST', numerator.first.toFixed(PRECISION));
+		resultString = resultString.replace('NUMERATOR_SECOND', numerator.second.toFixed(PRECISION));
+		// ПОДСТАНОВКА В ЗНАМЕНАТЕЛЬ
+		resultString = resultString.replace('DENOMINATOR', createDenominator(denominators));
+		return resultString;
+	}
+
+	function createSumP3(listOfIndex, k, j) {
+		var template = '{{\\rm P}(R_{INDEX_I} ){\\rm P}_{INDEX_K} \\left(R_{INDEX_I}^{*} (\\pi_{INDEX_J} )/R_{INDEX_I} \\right)}';
+		template = template.replace(/INDEX_J/g, j);
+		template = template.replace(/INDEX_K/g, k);
+		var resultString = "";
+		listOfIndex.forEach(function (value) {
+			resultString += template.replace(/INDEX_I/g, value) + '+';
+		});
+		return resultString.substring(0, resultString.length - 1);
+	}
+
+	//console.log(createCalcExempleP3(numerator, [numerator, numerator, numerator, numerator], 2, 11, 1, 5, 1, [1,2,5]));
+
+
+	function createSumP4(listOfIndex) {
+		var template = 'P(E_j)';
+		var resultString = '';
+		listOfIndex.forEach(function (value) {
+			resultString += template.replace('j', value) + '+';
+		});
+		return resultString.substring(0, resultString.length - 1);
+	}
+
+	/**
+	 * Олег
+	 * @param numerator числитель
+	 * @param denominators знаминатель, массив чисел
+	 * @param k индекс для P
+	 * @param i byltrc E
+	 * @param N до какого значения должна идти сумма в знаминателе
+	 * @param result результат вычислений
+	 */
+	function clarifyProbability(numerator, denominators, k, i, result) {
+
+		var listOfIndex = denominators.map(function (a) {
+			return a.stateNum;
+		});
+		
+		denominators = denominators.map(function (a) {
+			return a.probability;
+		});
+
+		var resultString = '\\[P_{INDEX_K}(E_{INDEX_I}) = \\frac{P(E_{INDEX_I})}{SUM} = \\frac{NUMERATOR}{DENOMINATOR} = RESULT\\]';
+		resultString = resultString.replace('RESULT', result.toFixed(PRECISION));
+		resultString = resultString.replace(/INDEX_I/g, i);
+		resultString = resultString.replace(/INDEX_K/g, k);
+		resultString = resultString.replace('NUMERATOR', numerator.toFixed(PRECISION));
+		resultString = resultString.replace('SUM', createSumP4(listOfIndex));
+		var denominatorResult = "";
+		denominators.forEach(function (value) {
+			denominatorResult += value.toFixed(PRECISION) + "+"
+		});
+		resultString = resultString.replace('DENOMINATOR', denominatorResult.substring(0, denominatorResult.length - 1));
+		return resultString;
+	}
+
+	//console.log(createCalcExempleP4(10, [1, 2, 3, 4], 1, 2, 3, 4));
+
+	function createSumP5(k, j, listOfIndex) {
+
+		var template = 'P_{INDEX_K}(E_{INDEX_I})P_{INDEX_K}(R_{INDEX_I}/(R^*_{INDEX_I}(\\pi_{INDEX_J}))';
+
+		template = template.replace(/INDEX_K/g, k);
+		template = template.replace(/INDEX_J/g, j);
+
+		var resultString = '';
+
+		listOfIndex.forEach(function (value) {
+			resultString += template.replace(/INDEX_I/g, value) + '+';
+		});
+
+		return resultString.substring(0, resultString.length - 1);
+	}
+
+	function D(k, j, result, calcValues, listOfIndex) {
+
+		var resultString = '\\[ D_{INDEX_K}(\\pi_{INDEX_J})~=~SUM~=~CALC~=~RESULT\\]';
+
+		resultString = resultString.replace(/INDEX_K/g, k);
+		resultString = resultString.replace(/INDEX_J/g, j);
+		resultString = resultString.replace('RESULT', result.toFixed(PRECISION));
+		resultString = resultString.replace('SUM', createSumP5(k, j, listOfIndex));
+
+		var calcResult = '';
+
+		calcValues.forEach(function (value) {
+			calcResult += value.first.toFixed(PRECISION) + ' + ' + value.second.toFixed(PRECISION) + '+';
+		});
+
+		resultString = resultString.replace('CALC', calcResult.substring(0, calcResult.length - 1));
+
+		return resultString
+	}
+
+	// console.log(createCalcExempleP5(5,4, 5,[numerator, numerator, numerator], [1,2,8]));
+}
